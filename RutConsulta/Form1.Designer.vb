5a1eabacbb5b052c5cc9391200420e5600652f82
alumnos.republica <alumnos.republica@REP-AL-RLC22-10>
2024-08-27 20:05:46 -0400

    Agregar archivos de proyecto.

---
+++ RutConsulta/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnBuscar = New Button()
+        txtRut = New TextBox()
+        txtResult = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnBuscar
+        ' 
+        btnBuscar.Location = New Point(465, 120)
+        btnBuscar.Name = "btnBuscar"
+        btnBuscar.Size = New Size(132, 98)
+        btnBuscar.TabIndex = 0
+        btnBuscar.Text = "Buscar Rut"
+        btnBuscar.UseVisualStyleBackColor = True
+        ' 
+        ' txtRut
+        ' 
+        txtRut.Location = New Point(204, 141)
+        txtRut.Name = "txtRut"
+        txtRut.Size = New Size(217, 23)
+        txtRut.TabIndex = 1
+        txtRut.Text = "Ingrese Rut a Buscar"
+        ' 
+        ' txtResult
+        ' 
+        txtResult.Location = New Point(204, 239)
+        txtResult.Name = "txtResult"
+        txtResult.Size = New Size(217, 23)
+        txtResult.TabIndex = 2
+        txtResult.Text = "Resultado de la Busqueda"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtResult)
+        Controls.Add(txtRut)
+        Controls.Add(btnBuscar)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents txtRut As TextBox
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents txtResultado As TextBox
+    Friend WithEvents Label1 As Label
+
+End Class
---
+++ RutConsulta/Form1.vb
@@ -0,0 +1,57 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles txtRut.TextChanged
+
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim rutToSearch As String = txtRut.Text
+        Dim excelFilePath As String = "C:\Users\alumnos.republica\Desktop\Maestra.xlsx"
+        Try
+            Dim excelApp As New Excel.Application
+            Dim workbook As Excel.Workbook = excelApp.Workbooks.Open(excelFilePath)
+            Dim worksheet As Excel.Worksheet = workbook.Sheets(1) ' Asegúrate de que el índice de la hoja sea correcto
+
+            Dim found As Boolean = False
+            Dim usedRange As Excel.Range = worksheet.UsedRange
+            For i As Integer = 1 To usedRange.Rows.Count
+                Dim rutCell As Excel.Range = usedRange.Cells(i, 1) ' Suponiendo que los RUT están en la primera columna
+                Dim nameCell As Excel.Range = usedRange.Cells(i, 2) ' Suponiendo que los nombres están en la segunda columna
+
+                If rutCell.Value IsNot Nothing AndAlso rutCell.Value.ToString() = rutToSearch Then
+                    txtResult.Text = nameCell.Value.ToString()
+                    found = True
+                    Exit For
+                End If
+            Next
+
+            If Not found Then
+                txtResult.Text = "RUT no encontrado"
+            End If
+
+            workbook.Close(False)
+            excelApp.Quit()
+
+            ' Liberar los recursos COM
+            ReleaseObject(worksheet)
+            ReleaseObject(workbook)
+            ReleaseObject(excelApp)
+
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ReleaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+End Class
